4240a1351039b862c30698d2656747f16230fbac
hemantnyadav <hemantnyadav@gmail.com>
2022-08-16 00:07:57 +0530

    expand with possible moves added to States class

---
+++ State.py
@@ -0,0 +1,45 @@
+class State:
+    goal = [1, 2, 3, 4, 5, 6, 7, 8, 0] 
+    #this should be changed manually based on n 
+    #e.g. it should be [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0] if n is 4.
+    
+    def __init__(self, state):
+        self.state = state
+
+    #this would remove illegal moves for a given state
+    def available_moves(x,n): 
+        moves = ['Left', 'Right', 'Up', 'Down']
+        if x % n == 0:
+            moves.remove('Left')
+        if x % n == n-1:
+            moves.remove('Right')
+        if x - n < 0:
+            moves.remove('Up')
+        if x + n > n*n - 1:
+            moves.remove('Down')
+
+        return moves
+
+    #produces children of a given state
+    def expand(self , n): 
+        x = self.state.index(0)
+        moves = self.available_moves(x,n)
+        
+        children = []
+        for direction in moves:
+            temp = self.state.copy()
+            if direction == 'Left':
+                temp[x], temp[x - 1] = temp[x - 1], temp[x]
+            elif direction == 'Right':
+                temp[x], temp[x + 1] = temp[x + 1], temp[x]
+            elif direction == 'Up':
+                temp[x], temp[x - n] = temp[x - n], temp[x]
+            elif direction == 'Down':
+                temp[x], temp[x + n] = temp[x + n], temp[x]
+              
+            children.append(State(temp, self, direction, self.depth + 1, 1)) #depth should be changed as children are produced
+
+        return children
+    
+
+     
